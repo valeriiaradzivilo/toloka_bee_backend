545b8ed53aa8b79bfb353766b4776814c66a053f
Valeriia Radzivilo <radzivilovaleriia@gmail.com>
2025-05-25 16:48:11 +0300

    Remove unnecessary comments and handle MongoDB connection exceptions silently

--- src/main/java/com/diplom/toloka_bee_backend/config/MongoConfig.java
+++ src/main/java/com/diplom/toloka_bee_backend/config/MongoConfig.java
@@ -27,15 +27,14 @@ public class MongoConfig {
                 .serverApi(serverApi)
                 .build();
 
-        // Create a new client and connect to the server
+
         try (MongoClient mongoClient = MongoClients.create(settings)) {
             try {
-                // Send a ping to confirm a successful connection
                 MongoDatabase database = mongoClient.getDatabase("admin");
                 database.runCommand(new Document("ping", 1));
                 System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
             } catch (MongoException e) {
-                e.printStackTrace();
+               //ignore
             }
         }
     }
